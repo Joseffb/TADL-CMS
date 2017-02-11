4ec163fb6fd98e1cafb7738c65ce9c42efc8bec8
JoseffB <joseffb@me.com>
2017-02-11 14:41:43 -0500

    first push

--- app/init.php
+++ app/init.php
@@ -2,6 +2,8 @@
 
 // Kickstart the framework
 $fw= require('../f3/base.php');
+if ((float)PCRE_VERSION<7.9)
+    trigger_error('PCRE version is out of date');
 
 //define global variables - start
 $fw->set( 'PUBLIC_ROOT', $fw->ROOT );
@@ -16,23 +18,6 @@ $fw->set( 'TEMPLATES_SITE', $fw->get( 'APP' ) . "ui/templates/" );
 $fw->set( 'TEMP', $fw->get( 'APP' ) . "temp/" );
 //define global variables - stop
 
-//start load config
-
 $fw->config($fw->CONFIGS."load.cfg");
-//stop stop config
-$controllerPath = $fw->APP;
 
-$t = new lib\core_cm();
-//$t->test();
-if ((float)PCRE_VERSION<7.9)
-    trigger_error('PCRE version is out of date');
-
-// todo before route check for required f3 libraries.
-//$fw->route('GET /',
-//    function($fw) {
-//        echo "<h3>Works</h3>";
-//    }
-//);
-//$fw->map('/json','lib\json');
-//$f3->set('ONERROR','Controller->method');
 $fw->run();
