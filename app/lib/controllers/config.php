3b9eb9e6439f764c861c1e1aeaaa63789b6959d5
JoseffB <joseffb@me.com>
2017-04-01 10:20:31 -0400

    New Config class to handle appending vs replacing system config vars

--- app/lib/controllers/config.php
+++ app/lib/controllers/config.php
@@ -4,45 +4,48 @@ namespace controllers;
 class config
 {
     private $fw = false;
+
     function __construct()
     {
         $this->fw = \Base::instance();
-        $this->fw->set('DEBUG', 4);
-        //echo "loading";
-            $this->fw->config($this->fw->CONFIGS . "user.cfg");
-            //echo "config loaded<br/>";
+        //$this->fw->set('DEBUG', 4);
     }
 
     static function load($cfg = false, $array_name = false)
     {
         $c = new config();
-        //echo 'load called';
         $c->append_autoloader($cfg, $array_name);
     }
 
-    function configs($cfg)
+    function config($cfg = false)
     {
-        $this->fw->config($cfg);
+        $cfg = $cfg ?: $this->fw->CONFIGS . "user.cfg";
+        if (file_exists($cfg)) {
+            $this->fw->config($cfg);
+            return true;
+        }
+        return false;
     }
 
     function append_autoloader($cfg = false, $array_name = false)
     {
-        if (!empty($cfg)) {
-            $this->configs($cfg);
+        if(!$this->config($cfg)) {
+            //no custom config file so we don't continue.
+            return false;
         }
-        $array_name= !empty($array_name)?$array_name:'user_load';
+        $array_name = !empty($array_name) ? $array_name : 'user_load';
         //remove it and then add it back.
         $retVal = "nope<br/>";
         $target = $this->fw->get($array_name);
-        if($target ) {
+        if ($target) {
             foreach ($target as $k => $v) {
                 $key = $this->fw->$k;
-                if($k == "AUTOLOAD") {
+                if ($k == "AUTOLOAD") {
                     //Autoload gets items appended to the string
                     //clean the string
                     $a = $this->fw->get("AUTOLOAD");
-                    $autoload = rtrim($a, ';') .';';
-                    $autoload .= (string)$v.';';
+                    $autoload = rtrim($a, ';') . ';';
+                    $autoload .= (string)$v . ';';
                     $this->fw->set("AUTOLOAD", $autoload);
                     $retVal = $this->fw->$k;
                 } else {
