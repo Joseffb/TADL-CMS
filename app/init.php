2de7002380bc4bbbaaf6f00197926fbc1d48fc3d
JoseffB <joseffb@me.com>
2017-04-17 07:38:53 -0400

    Tweaking documentation

--- app/init.php
+++ app/init.php
@@ -14,19 +14,19 @@ if ((float)PCRE_VERSION < 7.9)
 $fw->set('REALPATH', $path);
 $fw->set('ROOT', $fw->get('REALPATH'));
 $fw->set('PUBLIC_ROOT', $fw->get('REALPATH') . "public/");
-$fw->set('APP', $fw->get('ROOT') . "app/");
-$fw->set('CONFIGS', $fw->get('APP') . "configs/");
-$fw->set('PLUGINS', $fw->get('APP') . "extensions/");
-$fw->set('LIB', $fw->get('APP') . "lib/");
-$fw->set('CONTROLLERS', $fw->get('LIB') . "controllers/");
-$fw->set('CORE', $fw->get('LIB') . "core/");
-$fw->set('MODELS', $fw->get('LIB') . "models/");
-$fw->set('TABLES', $fw->get('LIB') . "tables/");
-$fw->set('UTIL', $fw->get('LIB') . "util/");
-$fw->set('UI', $fw->get('APP') . "ui/");
-$fw->set('THEMES_JSON', $fw->get('UI') . "json/");
-$fw->set('THEMES_EMAIL', $fw->get('UI') . "emails/");
-$fw->set('THEMES_SITE', $fw->get('UI') . "themes/");
+    $fw->set('APP', $fw->get('ROOT') . "app/");
+        $fw->set('CONFIGS', $fw->get('APP') . "configs/");
+        $fw->set('PLUGINS', $fw->get('APP') . "extensions/");
+        $fw->set('LIB', $fw->get('APP') . "lib/");
+            $fw->set('CONTROLLERS', $fw->get('LIB') . "controllers/");
+            $fw->set('CORE', $fw->get('LIB') . "core/");
+            $fw->set('MODELS', $fw->get('LIB') . "models/");
+            $fw->set('TABLES', $fw->get('LIB') . "tables/");
+            $fw->set('UTIL', $fw->get('LIB') . "util/");
+        $fw->set('UI', $fw->get('APP') . "ui/");
+            $fw->set('THEMES_JSON', $fw->get('UI') . "json/");
+            $fw->set('THEMES_EMAIL', $fw->get('UI') . "emails/");
+            $fw->set('THEMES_SITE', $fw->get('UI') . "themes/");
 //define global variables - stop
 
 //Do not modify this config unless you know what you're doing
